44032d36bcc8bc89d8b1d3b3d92df97b4e0413e5
leenenu <82006139+leenenu@users.noreply.github.com>
2021-07-20 17:14:03 +0800

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,24 +1,26 @@
-class numlist(object):
+import requests
+import json
+class citylist(object):
     def __init__(self):
-        self.counter=0
+        url = "https://chxin.neocities.org/weather.json"
+        r = requests.get(url)
+        w = json.loads(r.content)
+        self.w=w["weather"]
+        self.i=w["weather"][0]
+        self.n=0
     
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.counter +=1;
-        if self.counter >3:
+        if self.n >=len(self.w):
             raise StopIteration()
-        return self.counter-1
+        else :
+            self.i=self.w[self.n]
+            self.n =self.n+1;
+        return self.i
 
-obj= numlist()
-v1=next(obj)
-print(v1)
-v2=next(obj)
-print(v2)
-v3=next(obj)
-print(v3)
 
-obj2=numlist()
-for item in obj2:
-    print(item)
+obj=citylist()
+for item in obj:
+    print("城市:"+item["city"]+" 最低温度:"+str(item["mintmp"])+" 最高温度:"+str(item["maxtmp"])+" 天气:"+item["weather"])
